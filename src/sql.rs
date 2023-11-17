a7189450cd6c2b897e6860321882b82badfeae6c
Don Park <donp@donp.org>
2023-11-17 16:45:49 +0700

    compare pools based on equivalent eth

--- src/sql.rs
+++ src/sql.rs
@@ -69,7 +69,7 @@ pub async fn insert(mut db: Connection<AuthDb>, account: &Account) {
 }
 
 pub async fn top_pools(mut db: Connection<AuthDb>) -> Vec<Pool> {
-    let sql = "select pool_contract_address, sum(in0) as sum_in0, sum(in1) as sum_in1 from swaps group by pool_contract_address order by sum_in0 desc limit 10";
+    let sql = "select pool_contract_address, sum(in0_eth) as sum_in0, sum(in1_eth) as sum_in1 from swaps group by pool_contract_address order by sum_in0 desc limit 10";
     match sqlx::query(sql).fetch_all(&mut **db).await {
         Ok(rows) => {
             let mut r = vec![];
